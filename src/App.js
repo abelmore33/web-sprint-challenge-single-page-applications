da8e53d1454b8b3440569763301bd062eb994c83
Aaron Belmore <belmoreaaron9@gmail.com>
2023-04-08 20:35:32 -0400

    Completed checkboxes for toppings

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React from "react";
 import Home from "./components/Home";
 import Order from "./components/Order";
 import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 
 const emptyFormValues = {
   name: "",
   size: "",
   topping1: false,
   topping2: false,
+  topping3: false,
+  topping4: false,
+  topping5: false,
+  topping6: false,
+  topping7: false,
   special: "",
 };
+
 const App = () => {
+  const [formValues, setFormValues] = useState(emptyFormValues);
+
+  const handleChange = (name, value) => {
+    setFormValues({ ...formValues, [name]: value });
+  };
   return (
     <div className="app">
       <h1>Lambda Eats</h1>
@@ -22,7 +34,10 @@ const App = () => {
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="pizza" element={<Order />} />
+        <Route
+          path="pizza"
+          element={<Order values={formValues} change={handleChange} />}
+        />
       </Routes>
     </div>
   );
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,26 +1,58 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-function Order() {
+function Order(props) {
+  const { change, submit } = props;
+  const {
+    name,
+    size,
+    topping1,
+    topping2,
+    topping3,
+    topping4,
+    topping5,
+    topping6,
+    topping7,
+    special,
+  } = props.values;
+
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const newValue = type === "checkbox" ? checked : value;
+    change(name, newValue);
+  };
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
   return (
     <div>
       <h1>Build Your Own Pizza</h1>
       {/* img */}
-      <form id="pizza-form">
+      <form id="pizza-form" onSubmit={onSubmit}>
         <h2>Build Your Own Pizza</h2>
         <div className="name">
           <label htmlFor="name-input">Name: </label>
           <input
+            onChange={onChange}
             type="text"
             name="name"
             id="name-input"
             placeholder="Enter Name"
+            value={name}
           ></input>
         </div>
 
         <div className="pizzaSize">
           <h3>Choice of size</h3>
           <p>Required</p>
-          <select name="pizza" id="size-dropdown">
+          <select
+            onChange={onChange}
+            name="size"
+            id="size-dropdown"
+            value={size}
+          >
             <option value="">-Select A Size-</option>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
@@ -33,44 +65,82 @@ function Order() {
           <p>Choose up to 3</p>
           <label htmlFor="pepperoni">Pepperoni</label>
           <input
+            onChange={onChange}
+            checked={topping1}
             type="checkbox"
-            name="topping"
+            name="topping1"
             value="pepperoni"
             id="pepperoni"
           />
           <label htmlFor="chicken">Chicken</label>
-          <input type="checkbox" name="topping" value="chicken" id="chicken" />
+          <input
+            onChange={onChange}
+            checked={topping2}
+            type="checkbox"
+            name="topping2"
+            value="chicken"
+            id="chicken"
+          />
           <label htmlFor="pineapple">Pineapple</label>
           <input
+            onChange={onChange}
+            checked={topping3}
             type="checkbox"
-            name="topping"
+            name="topping3"
             value="pineapple"
             id="pineapple"
           />
           <label htmlFor="mushroom">Mushroom</label>
           <input
+            onChange={onChange}
+            checked={topping4}
             type="checkbox"
-            name="topping"
+            name="topping4"
             value="mushroom"
             id="mushroom"
           />
           <label htmlFor="peppers">Peppers</label>
-          <input type="checkbox" name="topping" value="peppers" id="peppers" />
+          <input
+            onChange={onChange}
+            checked={topping5}
+            type="checkbox"
+            name="topping5"
+            value="peppers"
+            id="peppers"
+          />
           <label htmlFor="bacon">Bacon</label>
-          <input type="checkbox" name="topping" value="bacon" id="bacon" />
-          <label htmlFor="pepperoni">sausage</label>
-          <input type="checkbox" name="topping" value="sausage" id="sausage" />
+          <input
+            onChange={onChange}
+            checked={topping6}
+            type="checkbox"
+            name="topping6"
+            value="bacon"
+            id="bacon"
+          />
+          <label htmlFor="sausage">Sausage</label>
+          <input
+            onChange={onChange}
+            checked={topping7}
+            type="checkbox"
+            name="topping7"
+            value="sausage"
+            id="sausage"
+          />
         </div>
         <div className="special-instructions">
           <h2>Any Special Request?</h2>
           <p>Enter Below:</p>
           <input
+            name="special"
+            onChange={onChange}
             type="text"
             id="special-text"
             placeholder="Enter request here"
+            value={special}
           ></input>
           <p>May increase price(will call to verify)</p>
         </div>
+        <button id="order-button">Place Order</button>
       </form>
     </div>
   );
